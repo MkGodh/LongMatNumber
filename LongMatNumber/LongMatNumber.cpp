78fd96a7518cccdda13afae39e37dc3f07ad1cb0
MkGodh <fenryrvla@yandex.ru>
2024-04-15 17:27:08 +0300

    Добавьте файлы проекта.

---
+++ LongMatNumber/LongMatNumber.cpp
@@ -0,0 +1,43 @@
+﻿#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+
+int main() {
+    using namespace std;
+    string num;
+    bool searchForMist = true;
+    bool hasDigit = false;
+
+    cout << "Please, input a number: ";
+    getline(cin, num);
+
+    int dotCount = 0;
+
+    for (int i = 0; i < num.length(); i++) {
+        if (num[i] == '.') {
+            dotCount++;
+        }
+
+        if (isdigit(num[i]) && num[i] >= '0' && num[i] <= '9') {
+            hasDigit = true;
+        }
+        if (!isdigit(num[i]) && num[i] != '.' && num[i] != '-') {
+            cout << "1";
+            searchForMist = false;
+            break;
+        }
+    }
+
+    if (searchForMist && hasDigit && dotCount <= 1) {
+        cout << "Yes!";
+    }
+    else {
+        cout << "No!";
+    }
+
+    return 0;
+}
